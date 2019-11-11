b8814f019798c03a8763b8765d9d5180509ee233
Luek9889 <lpearson345610@gmail.com>
2019-11-11 10:35:29 +0000

    user name and password

--- db-connect.php
+++ db-connect.php
@@ -1,8 +1,8 @@
 <?php
 
     $db_name = 'luke_products';
-    $db_user = 'root';
-    $db_pass = 'root';
+    $db_user = 'luke';
+    $db_pass = 'luek9889';
 
 
     $db  = new PDO("mysql:host=localhost;dbname=$da_name;charset=utf8mb4", $db_user, $db_pass);
